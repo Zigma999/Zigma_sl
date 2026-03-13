7508cc9c13ced6fc9687096c372242534df7cfde
Zigma999 <lelumwaruna51@gmail.com>
2026-03-13 14:19:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Zigma999/Zigma_sl/blob/main/images/Gemini_Generated_Image_lxytnqlxytnqlxyt%20copy.png?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 ,I'm Zigma_sl(MR.lelum's bot)  Alive Now😉😘😘*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
